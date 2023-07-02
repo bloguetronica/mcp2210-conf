ad050c84f49b3498df599c83e46fb2c1583bc75d
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2023-07-02 22:29:29 +0100

    Add files via upload

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -37,7 +37,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 protected:
