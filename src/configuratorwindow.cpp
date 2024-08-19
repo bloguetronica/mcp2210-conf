fe658db2b6086c9e59fbcbb289f3df7e893dc0c3
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2024-08-19 07:32:42 +0100

    Add files via upload

--- src/configuratorwindow.cpp
+++ src/configuratorwindow.cpp
@@ -666,7 +666,7 @@ quint32 ConfiguratorWindow::getNearestCompatibleBitRate(quint32 bitrate)
     MCP2210::SPISettings currentSPISettings = mcp2210_.getSPISettings(errcnt, errstr);  // Keep the current volatile SPI settings
     MCP2210::SPISettings testSPISettings = currentSPISettings;  // Settings used to test bitrate values
     quint32 testBitrate = static_cast<quint32>(1.5 * bitrate);  // Variable used for testing and finding compatible bit rates (multiplier value was determined empirically)
-    quint32 nearestLowerBitrate = MCP2210Limits::BITRATE_MIN, nearestUpperBitrate = MCP2210Limits::BITRATE_MAX;  // These variables are assigned here for correctness
+    quint32 nearestLowerBitrate = MCP2210Limits::BITRATE_MIN, nearestUpperBitrate = MCP2210Limits::BITRATE_MAX;  // These variables are assigned here, not just for correctness, but to allow algorithmic simplification
     while (errcnt == 0) {
         testSPISettings.bitrate = testBitrate;
         mcp2210_.configureSPISettings(testSPISettings, errcnt, errstr);
